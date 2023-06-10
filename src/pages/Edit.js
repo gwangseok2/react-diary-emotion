3a18bf678056cdbd413669dbb09efb1ff1e297f1
gs.song <rhrnrkd135@gmail.com>
2023-06-10 23:38:11 +0900

    [ + ] componet제작

---
+++ src/pages/Edit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Edit = () => {
+  return (
+    <div>
+      <h1>여기는 edit</h1>
+    </div>
+  );
+};
+
+export default Edit;
---
+++ src/pages/New.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const New = () => {
+  return (
+    <div>
+      <h1>여기는 New</h1>
+    </div>
+  );
+};
+
+export default New;
