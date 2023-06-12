d644a2a69c12cdd338536b8cbd3de7498c4cd46d
gs.song <rhrnrkd135@gmail.com>
2023-06-12 16:08:16 +0900

    [ + ] path variable 처리

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 import RouteTest from './components/RouteTest';
 
+/**
+ * 1. Path Variable
+ * /diary/1 -> 1번 일기
+ * useParams hooks 사용해
+ *
+ *
+ */
+
 function App() {
   return (
     <BrowserRouter>
@@ -18,6 +26,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/new" element={<New />} />
             <Route path="/edit" element={<Edit />} />
+            {/* 무조건 id가 있어야 Diary 컴포넌트를 불러옴. */}
+            <Route path="/diary/:id" element={<Diary />} />
+            {/* 이런식으로 예외처리 가능 */}
             <Route path="/diary" element={<Diary />} />
           </Routes>
           <RouteTest />
