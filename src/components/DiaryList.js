2b4dcc60071239198c27fc0bc6f34219b4d136dd
gs.song <rhrnrkd135@gmail.com>
2023-10-11 19:57:35 +0900

    [ + ] react select add

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,4 +1,22 @@
+import { useState } from 'react';
+
+const sortOptionList = [
+  { value: 'lastest', name: '최신순' },
+  { value: 'oldest', name: '오래된순' },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option>{it.name}</option>
+      ))}
+    </select>
+  );
+};
+
 const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType] = useState('lastest');
   return (
     <div>
       {diaryList.map((el) => {
@@ -11,6 +29,11 @@ const DiaryList = ({ diaryList }) => {
               borderBottom: '1px solid #ccc',
             }}
           >
+            <ControlMenu
+              value={sortType}
+              onChange={setSortType}
+              optionList={sortOptionList}
+            />
             일기ID : {el.id} <br />
             내용: {el.contents} <br />
             시간: {new Date(el.date).toLocaleString()} <br />
